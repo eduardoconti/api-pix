bcfc59d19fd52c6892a6989bbcb2e146075109f9
Eduardo Conti <eduardo.conti@gazin.com.br>
2023-02-26 21:27:11 -0300

    docs: adiciona @nestjs/swagger para documentação dos endpoints

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './main/main.module';
 import { EnvironmentVariables } from './main/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<EnvironmentVariables>);
+  const config = new DocumentBuilder()
+    .setTitle('API Pix')
+    .setDescription('API para gerar QrCode e processar pagamentos com `PIX`')
+    .setVersion('1.0')
+    .addTag('health-check')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(configService.get('PORT') | 3000);
 }
 bootstrap();
--- src/presentation/controllers/health-check.controller.ts
+++ src/presentation/controllers/health-check.controller.ts
@@ -1,8 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health-check')
 @Controller()
 export class HealthCheckController {
   @Get()
+  @ApiOkResponse({
+    description: 'Health check',
+    schema: {
+      type: 'string',
+      example: 'app is running',
+    },
+  })
   handle(): string {
     return 'app is running';
   }
