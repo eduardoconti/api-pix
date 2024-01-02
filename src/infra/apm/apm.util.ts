d5e056dd4924f0a1cd594cca70010f4d46472f73
Eduardo Conti <es.eduardoconti@gmail.com>
2024-01-02 15:47:36 -0300

    refactor: ajuste import apm

--- src/infra/apm/apm.util.ts
+++ src/infra/apm/apm.util.ts
@@ -1,10 +1,9 @@
 import * as APM from 'elastic-apm-node';
+import apm from 'elastic-apm-node';
 
 let instance: APM.Agent | undefined;
 
 export const initializeAPMAgent = (config?: APM.AgentConfigOptions): void => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const apm = require('elastic-apm-node');
   instance = config ? apm.start(config) : apm.start();
 };
 
